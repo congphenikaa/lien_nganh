fe7850d1692a4c5822954fae35e6c040dd11276d
Cong <huucong12a2mda@gmail.com>
2025-09-21 18:46:41 +0700

    deploy len vervel và làm thêm logic của thêm lớp, xem lớp, và bảng cho người giảng dạy, thêm được lưu trữ ảnh, video trên cloudary

--- server/controllers/educatorController.js
+++ server/controllers/educatorController.js
@@ -1,4 +1,7 @@
 import { clerkClient} from '@clerk/express'
+import Course from '../models/Course.js'
+import { v2 as cloudinary } from 'cloudinary'
+import { Purchase } from '../models/Purchae.js'
 
 export const updateRoleToEducator = async (req, res) => {
     try {
@@ -12,5 +15,82 @@ export const updateRoleToEducator = async (req, res) => {
         
     } catch (error) {
         res.json({success: false, message: error.message})
+    }
+}
+
+export const addCourse = async (req, res) => {
+    try{
+        const {courseData} = req.body
+        const imageFile = req.file
+        const educatorId = req.auth.userId
+
+        if (!imageFile) {
+            return res.json({ success: false, message: 'Thumbnail Not Attached' })
+        }
+
+        const parsedCourseData = await JSON.parse(courseData)
+        parsedCourseData.educator = educatorId
+        const newCourse = await Course.create(parsedCourseData)
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path)
+        newCourse.courseThumbnail = imageUpload.secure_url
+        await newCourse.save()
+
+        res.json({ success: true, message: 'Course Added Successfully'})
+
+
+    }catch (error) {
+        res.json({ success: false, message: error.message})
+    }
+}
+
+export const getEducatorCourses = async (req, res) => {
+    try {
+        const educator = req.auth.userId
+        const courses = await Course.find({educator})
+        res.json({success:true, courses})
+
+    }catch (error) {
+        res.json({success:false, message: error.message})
+    }
+}
+
+// Get educator dashborad data {total earning, entolled students , No. of Courses}
+
+export const educatorDashboard = async () => {
+    try {
+        const educator = rep.auth.userId;
+        const courses = await Course.find({educator});
+        const totalCourses = courses.length;
+
+        const courseIds = courses.map((course) => course._id);
+
+        // Calculate total earnings from purchases
+        const purchases = await Purchase.find({
+            courseId: { $in: courseIds},
+            status: 'completed'
+        });
+
+        const totalEarnings = purchases.reduce((sum, purchase) => sum + purchase.amount, 0);
+
+        // Collect unique enrooled students Ids with their couse titles
+        const enrolledStudentsData = [];
+        for(const course of courses) {
+            const students = await User.find({
+                _id: {$in: course.enrolledStudents}
+            }, 'name ImageUrl');
+
+            students.forEach((student => {
+                enrolledStudentsData.push({
+                    courseTitle: course.courseTitle,
+                    student
+                });
+            }));
+
+            res.json({success: true, dashboardData: {
+                totalEarnings, enrolledStudentsData, totalCourses
+            }})
+        }
+    }catch (error) {
+        res.json({success:false, message: error.message});
     }
 }
