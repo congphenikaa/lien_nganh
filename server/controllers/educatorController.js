7f00c01dafd1157927815d08cb6c9c2f58af5af6
SyDong2k4 <nguyensydong21112004@gmail.com>
2025-09-28 10:41:55 +0700

    backend complete

--- server/controllers/educatorController.js
+++ server/controllers/educatorController.js
@@ -56,7 +56,7 @@ export const getEducatorCourses = async (req, res) => {
 
 // Get educator dashborad data {total earning, entolled students , No. of Courses}
 
-export const educatorDashboard = async () => {
+export const educatorDashboardData = async (req, res) => {
     try {
         const educator = rep.auth.userId;
         const courses = await Course.find({educator});
@@ -93,4 +93,31 @@ export const educatorDashboard = async () => {
     }catch (error) {
         res.json({success:false, message: error.message});
     }
+}
+
+// Get Enrolled Students Data with Purchase Data
+
+export const getEnrolledStudentsData = async (req, res)=> {
+    try {
+        const educator = req.auth.userId;
+        const courses = await Course.find({educator});
+        const courseIds = courses.map(course => course._id);
+
+        const purchases = await Purchase.find({
+            courseId: {$in: courseIds},
+            status: 'completed'
+        }).populate('userId', 'name imageUrl').populate('courseId', 'courseTitle')
+
+        const enrolledStudents = purchases.map(purchase => ({
+            student: purchase.userId,
+            courseTitle: purchase.courseId.courseTitle,
+            purchaseDate: purchase.createdAt
+        }));
+
+        res.json({success: true, enrolledStudents})
+        
+    } catch (error) {
+        res.json({success:false, message: error.message});
+    }
+
 }
